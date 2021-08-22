3b0a0d691601199da14b2c770ac060dcc2758439
Rick Hallman <rhallman96@gmail.com>
2021-08-21 23:42:41 -0700

    Add scaffolding.

--- system/mmc3.go
+++ system/mmc3.go
@@ -1,18 +1,19 @@
 package system
 
 // mmc3 CPU banks
-//
-//
-//
-// mmc3 PPU banks
-//
-//
-//
+// 0x6000 - 0x7fff - switchable PRG RAM bank
+// 0x8000 - 0x9fff - switchable PRG ROM bank
+// 0xa000 - 0xbfff - switchable PRG ROM bank
+// 0xc000 - 0xdfff - mirror of 0x8000 - 0x9fff
+// 0xe000 - 0xffff - bank fixed to last PRG ROM bank
 
 type mmc3 struct {
 	prgROM []uint8
 	prgRAM []uint8
 	chr    []uint8
+
+	bankRegIndex uint8
+	bankRegs     []uint8
 
 	mirror mirrorMode
 }
